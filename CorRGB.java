b5e4cdd75523e06d89df749dd9379ae7cf88b174
JonatasSa-prog <60881191+JonatasSa-prog@users.noreply.github.com>
2021-03-09 10:34:52 -0300

    Update CorRGB.java

--- CorRGB.java
+++ CorRGB.java
@@ -5,9 +5,9 @@ public class CorRGB {
     private double luminosidade;
     
     public CorRGB(int red, int green, int blue) {
-        this.red = red;
-        this.green = green;
-        this.blue = blue;
+        this.setRed(red);
+        this.setGreen(green);
+        this.setBlue(blue);
     }
 
     public int getRed() {
@@ -15,7 +15,12 @@ public class CorRGB {
     }
 
     public void setRed(int red) {
-        this.red = red;
+        if(red >= 0 && red <=255)
+            this.red = red;
+        else{
+            System.out.println("Error! Valor Incorreto, " + red + " não está dentro do conjunto numérico."); 
+            System.exit(0);
+        }
     }
 
     public int getGreen() {
@@ -23,7 +28,13 @@ public class CorRGB {
     }
 
     public void setGreen(int green) {
-        this.green = green;
+        if(green >= 0 && green <= 255)
+            this.green = green;
+        else{
+            System.out.println("Error! Valor Incorreto, " + green + " não está dentro do conjunto numérico."); 
+            System.exit(0);
+        }
+        
     }
 
     public int getBlue() {
@@ -31,7 +42,12 @@ public class CorRGB {
     }
 
     public void setBlue(int blue) {
-        this.blue = blue;
+        if(blue >= 0 && blue <=255)
+            this.blue = blue;
+        else{
+            System.out.println("Error! Valor Incorreto, " + blue + " não está dentro do conjunto numérico."); 
+            System.exit(0);
+        }
     }
 
     public double getLuminosidade(){
